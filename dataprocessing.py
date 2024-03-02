f66a381b86438037284b7d65d13a531214a502ec
Arti Jain <artijain@Artis-MacBook-Pro.local>
2024-03-02 00:42:41 -0500

    notebooks added

--- dataprocessing.py
+++ dataprocessing.py
@@ -19,7 +19,7 @@ def excel_to_txt(excel_file_path, output_file_name, output_file_dir):
         
     Output File Format:
         SYNONYMS = {
-            "These are my four associated words: badgers, bugs, hounds, nags": "pester",
+            "These are my four associated words: badgers, bugs, hounds, nags": "pester", ...
     """
     if not os.path.exists(excel_file_path):
         raise FileNotFoundError(f"The file, {excel_file_path} does not exist.")
@@ -55,8 +55,59 @@ def excel_to_txt(excel_file_path, output_file_name, output_file_dir):
             
     logging.info(f"Output file written to: {output_file}")
 
+
+def excel_to_txt_files(excel_file_path, output_file_dir):
+    """
+    This function will take in an input data spreadsheet and output a folder of .txt files with the data - one file per category. 
+    This is to organize the data before tokenization.
+    The output will be split by category: synonyms, pop_culture, thematic, word_based, fitb (fill in the blank)
+    
+    Inputs:
+        excel_file_path - this is the path to the original excel spreadsheet with the data
+        output_file_dir - this is the name of the directory where the output text file will be saved
+        
+    Returns:
+        None
+        
+    Output File Format:
+        {"These are my four associated words: badgers, bugs, hounds, nags": "pester", ...
+    """
+    if not os.path.exists(excel_file_path):
+        raise FileNotFoundError(f"The file, {excel_file_path} does not exist.")
+    
+    if not os.path.exists(output_file_dir):
+        logging.debug(f"Output file dir: {output_file_dir} does not exist. Creating file in {os.getcwd()}/preprocessed_data")
+        os.mkdir(f"{os.getcwd()}/preprocessed_data")
+        output_file_dir = f"{os.getcwd()}/preprocessed_data"
+                
+    df = pd.read_excel(excel_file_path)
+    
+    categories = ["SYNONYMS", "POP_CULTURE", "THEMATIC", "WORD_BASED", "FITB"]
+    category_dictionary = {}
+    for _, row in df.iterrows():
+        if row[0] in categories:        
+            if category_dictionary != {}:
+                output_file = os.path.join(output_file_dir, f"{heading}.txt")
+                with open(output_file, 'a') as outfile:
+                    outfile.write(json.dumps(category_dictionary, indent=2))
+            category_dictionary = {}
+            heading = row[0]
+        else:
+            string_split = row[0].split('"')
+            string_split = string_split[1].split(":")
+            key = row[1].split('"')
+            key = key[1].lower()
+            category_dictionary[f"These are my four associated words:"+string_split[1].lower()] = key
+            
+    # last category
+    output_file = os.path.join(output_file_dir, f"{heading}.txt")
+    with open(output_file, 'w') as outfile:
+        outfile.write(json.dumps(category_dictionary, indent=2))
+            
+    logging.info(f"Output file written to: {output_file}")
             
         
 if __name__ == "__main__":
-    excel_to_txt("./original_dataset.xlsx", "originaldata", "./preprocessed_data")
+    #excel_to_txt("./original_dataset.xlsx", "originaldata", "./preprocessed_data")
+    excel_to_txt_files("./original_dataset.xlsx", "./preprocessed_data")
         
